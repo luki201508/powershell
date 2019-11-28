d6d39467b1c9c9606c88ea7a4e8675def04320d0
unknown <luki201508@gmail.com>
2019-11-28 11:19:50 +0100

    Ejer 6.1 terminado

--- ejer6-1/6-1-2.bat
+++ ejer6-1/6-1-2.bat
@@ -1,3 +1,15 @@
 @echo off
-
+REM Crear un procedimiento bat en 
+REM el que se pida un nombre de directorio, 
+REM se cree y se situe en él
+:SCRIPTSTART
+set /p path="Escriba un nombre de directorio: "
+IF EXIST %CD%\%path% (
+    echo Ese directorio ya existe.
+    GOTO SCRIPTSTART
+) ELSE (
+    echo Se está creando el directorio en %CD%\%path%
+    mkdir %CD%\%path%
+    start /d "%CD%\%path%"
+)
 exit
---
+++ ejer6-1/6-1-3.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Hacer un procedimiento de
+REM comandos que pida dos números y 
+REM dé su suma, la resta, y la multiplicación de ambos.
+set /p x="Ponga el primer numero: "
+set /p y="Ponga el segundo numero: "
+set /a total="%x%+%y%"
+echo "La suma es %total%"
+set /a total="%x%-%y%"
+echo "La resta es %total%"
+set /a total="%x%*%y%"
+echo "La multiplicacion es %total%"
+exit
